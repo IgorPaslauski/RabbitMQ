23b1b5e8874b361cc9172f7cc39edc3bae11b607
igorp <io006235@cfjl.com.br>
2024-06-17 23:56:28 -0300

    primeiro código utilizando rabbitmq

---
+++ Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+        using var connection = connectionFactory.CreateConnection();
+        using var model = connection.CreateModel();
+        
+        model.ExchangeDeclare("exchange-name", ExchangeType.Direct);
+        
+        var queueName = model.QueueDeclare().QueueName;
+        model.QueueBind(queueName, "exchange-name", "routing-key");
+        
+        var consumer = new EventingBasicConsumer(model);
+        consumer.Received += (sender, eventArgs) =>
+        {
+            var body = eventArgs.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Received message: {message}");
+        };
+        
+        model.BasicConsume(queueName, true, consumer);
+        
+        Console.WriteLine("Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
